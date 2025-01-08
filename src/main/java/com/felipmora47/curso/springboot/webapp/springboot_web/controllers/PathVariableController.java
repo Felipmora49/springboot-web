3a2ac014f7a90cecd18087d4e04bb97c1f041dd3
felipmora47 <felipmora43@gmail.com>
2025-01-08 16:53:49 -0500

    Envio de Multiples Parametros con @ParthVariable & Envio de JSON  Peticion POST con ApiREST

--- src/main/java/com/felipmora47/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/felipmora47/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,10 +3,18 @@ package com.felipmora47.curso.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.felipmora47.curso.springboot.webapp.springboot_web.models.User;
 import com.felipmora47.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
@@ -20,5 +28,28 @@ public class PathVariableController {
         param.setMessage(message);
         return param;
     }
+    
+
+
+    @GetMapping("/mix-variables/{id}/{product}/{price}")
+    public Map<String, Object> mixPathVar(@PathVariable Integer id, @PathVariable String product, @PathVariable Long price){
+
+        Map<String,Object> json = new HashMap<>();
+        json.put("id", id);
+        json.put("product", product);
+        json.put("price", price);
+        return json;
+    }
+    
+
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+
+        //Hacer algo con el usuario recibido en la BD
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+    
     
 }
