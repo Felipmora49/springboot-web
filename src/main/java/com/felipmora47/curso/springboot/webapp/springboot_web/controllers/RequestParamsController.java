3297cc7bf4ee6abf89723ae9715771c032ba1a0f
felipmora47 <felipmora43@gmail.com>
2025-01-08 16:36:14 -0500

    Anotacion @PathVariable & Creacion de ParamMixDto

--- src/main/java/com/felipmora47/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/felipmora47/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipmora47.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+import com.felipmora47.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -26,16 +27,16 @@ public class RequestParamsController {
     }
 
     @GetMapping("/param-bar")
-    public ParamDto paramBar(@RequestParam String text, @RequestParam Integer code){
+    public ParamMixDto paramBar(@RequestParam String text, @RequestParam Integer code){
 
-        ParamDto params = new ParamDto();
+        ParamMixDto params = new ParamMixDto();
         params.setMessage(text);
         params.setCode(code);
         return params;
     }
     
     @GetMapping("/request")
-    public  ParamDto request(HttpServletRequest request){
+    public  ParamMixDto request(HttpServletRequest request){
         Integer code = 0;
 
         try 
@@ -44,7 +45,7 @@ public class RequestParamsController {
         } 
         catch (NumberFormatException e) {
         }
-        ParamDto params = new ParamDto();
+        ParamMixDto params = new ParamMixDto();
         params.setCode(code);
         params.setMessage(request.getParameter("message"));
         return params;
--- src/main/java/com/felipmora47/curso/springboot/webapp/springboot_web/models/dto/ParamMixDto.java
+++ src/main/java/com/felipmora47/curso/springboot/webapp/springboot_web/models/dto/ParamMixDto.java
@@ -1,9 +1,10 @@
 package com.felipmora47.curso.springboot.webapp.springboot_web.models.dto;
 
-public class ParamDto {
+public class ParamMixDto {
 
     private String message;
-    
+    private Integer code;
+
     public String getMessage() {
         return message;
     }
@@ -11,4 +12,14 @@ public class ParamDto {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    
 }
